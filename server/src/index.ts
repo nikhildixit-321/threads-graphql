db77995515baed6ec87c6e9cb458b799056859a6
Nikhil Dixit <170299140+nikhildixit-321@users.noreply.github.com>
2026-06-14 15:14:01 +0530

    prisma add

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@as-integrations/express5';
+import prisma from "./lib/db.js";
 const app = express();
 
 // create graphql server 
@@ -11,12 +12,29 @@ async function init(){
     hello: String
     say(name: String): String
   }
+  type Mutation {
+   createUser(email:String!,firstName:String!,lastName:String!,password:String! ): Boolean}
 `,
 resolvers: {
   Query: {
     hello: () => "Hello, GraphQL!",
     say:(_,{name}:{name: string}) => `Hello, ${name}!`
-  }
+  },
+  Mutation:{
+    createUser: async (_,{email,firstName,lastName,password }:
+      {email:string,firstName:string,  lastName:string,password:string}) =>{
+      await prisma.user.create({
+        data:{
+          email:email,
+          firstName:firstName,
+          lastName:lastName,
+          passward:password,
+          salt :'randdom_salt' 
+        }
+      });
+      return true;
+    }
+  } 
 }
 });
 // start the gql server 
